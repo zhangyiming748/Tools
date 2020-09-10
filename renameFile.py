f5d8937ecf99ed95e4bb1b90efacc9d5ec3f93e0
zen <zhangyiming748@gmail.com>
2020-09-10 23:33:35 +0800

    修改文件名中的中文符号

---
+++ renameFile.py
@@ -0,0 +1,46 @@
+# 重命名文件替换中文符号
+# 在macOS/Linux下正常运行
+import os
+
+
+# 获取文件名(列表)
+
+def getFilesName(path):
+    fileList = os.listdir(path)
+    return fileList
+
+
+# 替换中文符号生成新文件名
+def replace(oldName):  # 【上午】
+    oldName = oldName.replace('，', ',')
+    oldName = oldName.replace('。', '.')
+    oldName = oldName.replace('（', '(')
+    oldName = oldName.replace('）', ')')
+    oldName = oldName.replace('“', '\"')
+    oldName = oldName.replace('”', '\"')
+    oldName = oldName.replace('：', ':')
+    oldName = oldName.replace('；', ';')
+    oldName = oldName.replace('？', '?')
+    oldName = oldName.replace('！', '!')
+    oldName = oldName.replace('《', '<')
+    oldName = oldName.replace('》', '>')
+    oldName = oldName.replace('【', '[')
+    oldName = oldName.replace('】', ']')
+    oldName = oldName.replace('、', '\\')
+    oldName = oldName.replace('～', '~')
+    # print("生成的文件名是 %s"%oldName)
+    newName = oldName
+    return newName
+
+
+if __name__ == '__main__':
+    path = '/Volumes/Samsung/text/'
+    names = getFilesName(path=path)
+    for name in names:
+        print('获得的旧文件名 %s' % name)
+        print(type(name))
+        new = replace(name)
+        print('获得的新文件名 %s' % new)
+        print(type(new))
+        # os.rename(name,new)
+        os.rename(os.path.join(path, name), os.path.join(path, new))
