38cf8a8bd498cb3b581b6a3968d1a5b8c8120794
zen <zhangyiming748@gmail.com>
2020-09-24 15:22:40 +0800

    禁用前清除缓存

--- disableApp.sh
+++ disableApp.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 # adb shell pm disable-user
 
-# 停用软件更新
+echo 清除软件更新缓存
 adb shell pm clear com.huawei.android.hwouc
+echo 停用软件更新
 adb shell pm disable-user com.huawei.android.hwouc
-# 停用华为视频
+echo 清除华为视频缓存
 adb shell pm clear com.huawei.himovie
+echo 停用华为视频
 adb shell pm disable-user com.huawei.himovie
-# 停用华为音乐
+echo 清除华为音乐缓存
 adb shell pm clear com.android.mediacenter
+echo 停用华为音乐视频
 adb shell pm disable-user com.android.mediacenter
-# 停用华为浏览器
+echo 清除华为浏览器缓存
 adb shell pm clear com.huawei.browser
+echo 清除华为浏览器缓存
 adb shell pm disable-user com.huawei.browser
-# 华为阅读
+echo 清除华为阅读缓存
 adb shell pm clear com.huawei.hwireader
+echo 清除华为阅读缓存
 adb shell pm disable-user com.huawei.hwireader
-# 华为主题
+echo 清除华为主题缓存
 adb shell pm clear com.huawei.android.thememanager
+echo 清除华为主题缓存
 adb shell pm disable-user com.huawei.android.thememanager
